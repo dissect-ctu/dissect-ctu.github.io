e75bc85bf35e4601762d241eb0ac25b6635016d0
Tomáš Stefan <ts@stdin.cz>
2019-09-21 11:52:58 +0200

    Landing page is informational, blog moved to /blog/, style improvements, other
     - changed format of members yml file
     - landing page is the one with info about team, blog is in /blog/
     - add members page
     - style polishing
     - new appearance of toggling blog post preview

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,38 @@
-// Show an element
-var show = function (elem) {
-	elem.classList.add('is-visible');
-};
-
-// Hide an element
-var hide = function (elem) {
-	elem.classList.remove('is-visible');
-};
-
-// Toggle element visibility
 var toggle = function (elem) {
 	elem.classList.toggle('is-visible');
 };
+
+
+var toggle_arrow_icon = function(arrow, content) {
+	
+	if (content.offsetParent === null) {
+		arrow.classList.remove('fa-arrow-circle-up');
+		arrow.classList.add('fa-arrow-circle-down');
+	} else {
+		arrow.classList.remove('fa-arrow-circle-down');
+		arrow.classList.add('fa-arrow-circle-up');
+	}
+}
 
 // Listen for click events
 document.addEventListener('click', function (event) {
 
 	// Make sure clicked element is our toggle
-	if (!event.target.classList.contains('preview-toggle')) return;
+	if (!event.target.closest('a').classList.contains('preview-toggle')) {
+		return;
+	}
 
 	// Prevent default link behavior
 	event.preventDefault();
 
-	// Get the content
-	var content = document.querySelector(event.target.hash);
-	if (!content) return;
-
 	// Toggle the content
+	var content = document.querySelector(event.target.closest('a').hash);
+	if (!content) return;
 	toggle(content);
+
+	// Toggle arrow icon (up/down) according to content
+	var arrow_icon = event.target.querySelector('i');
+	if (!arrow_icon) return;
+	toggle_arrow_icon(arrow_icon, content);
 
 }, false);
